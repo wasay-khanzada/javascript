c1bb0997101fafa4f370a7f804449b4fb9cf5bd1
wasay-khanzada <waseykhanzada123@gmail.com>
2024-06-27 21:41:42 +0000

    live

---
+++ 1-basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12234
+let accountEmail = "waseykhanzada@gmail.com"
+var accountNo = "101010"
+accountState = "karachi"
+let accoountName
+
+// accountId = 12234
+accountEmail = "waseykhanzada123@gmail.com"
+accountNo = "191919"
+accountState = "sinddh"
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountNo, accountState, accoountName]);
